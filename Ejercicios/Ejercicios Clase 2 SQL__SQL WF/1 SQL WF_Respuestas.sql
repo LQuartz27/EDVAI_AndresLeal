d73e30a9bf4e237f580db8d946ae9088b122f2b2
LQuartz27 <andres05.leal@gmail.com>
2023-10-23 20:47:18 -0500

    Added the statements to the SQL WF answers file

--- Ejercicios/Ejercicios Clase 2 SQL__SQL WF/1 SQL WF_Respuestas.sql
+++ Ejercicios/Ejercicios Clase 2 SQL__SQL WF/1 SQL WF_Respuestas.sql
@@ -1,4 +1,9 @@
 --1
+/*
+Obtener el promedio de precios por cada categoría de producto. La cláusula
+OVER(PARTITION BY CategoryID) específica que se debe calcular el promedio de
+precios por cada valor único de CategoryID en la tabla.
+*/
 select
 	c.category_name ,
 	p.product_name ,
@@ -12,6 +17,9 @@ inner join categories c
 
 
 --2
+/*
+Obtener el promedio de venta de cada cliente
+*/
 select
 	avg(unit_price * quantity) over(partition by o.customer_id) as avgorderamount,
 	o.order_id ,
@@ -28,6 +36,11 @@ order by o.customer_id;
 
 
 --3
+/*
+Obtener el promedio de cantidad de productos vendidos por categoría (product_name,
+quantity_per_unit, unit_price, quantity, avgquantity) y ordenarlo por nombre de la
+categoría y nombre del producto
+*/
 select
 	p.product_name ,
 	c.category_name ,
@@ -47,6 +60,10 @@ order by 2,1;
 
 
 --4
+/*
+Selecciona el ID del cliente, la fecha de la orden y la fecha más antigua de la
+orden para cada cliente de la tabla 'Orders'.
+*/
 select
 	c.customer_id ,
 	o.order_date ,
@@ -59,6 +76,10 @@ order by customer_id ;
 
 
 --5
+/*
+Seleccione el id de producto, el nombre de producto, el precio unitario, el id de
+categoría y el precio unitario máximo para cada categoría de la tabla Products.
+*/
 select
 	p.product_id ,
 	p.product_name ,
@@ -71,6 +92,9 @@ order by p.category_id ;
 
 
 --6
+/*
+Obtener el ranking de los productos más vendidos
+*/
 select
 	row_number() over(order by sum(od.quantity) desc),
 	p.product_name ,
@@ -84,6 +108,9 @@ group by p.product_name ;
 
 
 --7
+/*
+Asignar numeros de fila para cada cliente, ordenados por customer_id
+*/
 select
 	row_number() over(order by customer_id) as ranking,
 	c.customer_id ,
@@ -96,6 +123,10 @@ from
 
 
 --8
+/*
+Obtener el ranking de los empleados más jóvenes () ranking, nombre y apellido del
+empleado, fecha de nacimiento)
+*/
 select
 	row_number() over(order by e.birth_date desc) as ranking,
 	concat(e.first_name, ' ', e.last_name)  as employee_name,
@@ -105,6 +136,9 @@ from
 
 
 --9
+/*
+Obtener la suma de venta de cada cliente
+*/
 select
 	sum(unit_price * quantity) over(partition by o.customer_id) as client_total_amount,
 	o.order_id ,
@@ -119,6 +153,9 @@ on o.order_id = od.order_id ;
 
 
 --10
+/*
+Obtener la suma total de ventas por categoría de producto
+*/
 select
 	c.category_name ,
 	p.product_name ,
@@ -137,6 +174,10 @@ order by category_name , p.product_name ;
 
 
 --11
+/*
+Calcular la suma total de gastos de envío por país de destino, luego ordenarlo por país
+y por orden de manera ascendente
+*/
 select
 	c.country ,
 	o.order_id ,
@@ -156,6 +197,9 @@ order by
 
 
 --12
+/*
+Ranking de ventas por cliente
+*/
 select
 	o.customer_id  ,
 	c.company_name,
@@ -171,6 +215,9 @@ group by o.customer_id ,c.company_name ;
 
 
 --13
+/*
+Ranking de empleados por fecha de contratacion
+*/
 select
 	e.employee_id ,
 	e.first_name ,
@@ -182,6 +229,9 @@ from
 
 
 --14
+/*
+Ranking de productos por precio unitario
+*/
 select
 	p.product_id ,
 	p.product_name ,
@@ -192,6 +242,10 @@ from
 
 
 --15
+/*
+Mostrar por cada producto de una orden, la cantidad vendida y la cantidad
+vendida del producto previo.
+*/
 select
 	od.order_id ,
 	p.product_id ,
@@ -204,6 +258,10 @@ p.product_id = od.product_id;
 
 
 --16
+/*
+Obtener un listado de ordenes mostrando el id de la orden, fecha de orden, id del cliente
+y última fecha de orden
+*/
 select
 	o.order_id ,
 	o.order_date ,
@@ -214,6 +272,11 @@ from
 
 
 --17
+/*
+Obtener un listado de productos que contengan: id de producto, nombre del producto,
+precio unitario, precio del producto anterior, diferencia entre el precio del producto y
+precio del producto anterior
+*/
 select
 	p.product_id ,
 	p.product_name ,
@@ -225,6 +288,10 @@ from
 
 
 --18
+/*
+Obtener un listado que muestra el precio de un producto junto con el precio del producto
+siguiente
+*/
 select
 	p.product_name ,
 	p.unit_price ,
@@ -234,6 +301,10 @@ from
 
 
 --19
+/*
+Obtener un listado que muestra el total de ventas por categoría de producto junto con el
+total de ventas de la categoría siguiente
+*/
 select
 	c.category_name ,
 	sum(od.quantity * od.unit_price) as total_sales ,
